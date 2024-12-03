c712e4f7e23b43481af131346225967f1cc6a9c4
Danzrrr <graita.sukma@gmail.com>
2024-12-03 23:35:53 +0700

    feat: base unit test

--- src/ERC20Scratch.sol
+++ src/ERC20Scratch.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: MIT
+// THIS CODE IS FOR LEARNING PURPOSES AND DEMONSTRATES THE BASIC FUNCTIONALITY OF AN ERC20 TOKEN.
 
 pragma solidity >=0.8.25;
+
+error InsufficientBalance(uint256 available, uint256 required);
+error InsufficientAllowance(uint256 available, uint256 required);
+error AccountError(string message);
 
 contract ERC20 {
     string private _name;
@@ -37,5 +42,57 @@ contract ERC20 {
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return _balances[_owner];
+    }
+
+    function transfer(address _to, uint256 _amount) public returns (bool success) {
+        if (_balances[msg.sender] < _amount) {
+            revert InsufficientBalance({ available: _balances[msg.sender], required: _amount });
+        }
+        if (_to == address(0)) {
+            revert AccountError("cannot transfer to zero address");
+        }
+
+        _balances[msg.sender] -= _amount;
+        _balances[_to] += _amount;
+
+        emit Transfer(msg.sender, _to, _amount);
+        return true;
+    }
+
+    function approve(address _spender, uint256 _amount) public returns (bool success) {
+        _allowances[msg.sender][_spender] = _amount;
+
+        emit Approval(msg.sender, _spender, _amount);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return _allowances[_owner][_spender];
+    }
+
+    function transferFrom(address _from, address _to, uint256 _amount) public returns (bool success) {
+        if (_balances[_from] < _amount) {
+            revert InsufficientBalance({ available: _balances[_from], required: _amount });
+        }
+        if (_allowances[_from][msg.sender] < _amount) {
+            revert InsufficientAllowance({ available: _allowances[_from][msg.sender], required: _amount });
+        }
+        if (_to == address(0)) {
+            revert AccountError("cannot transfer to zero address");
+        }
+
+        _balances[_from] -= _amount;
+        _balances[_to] += _amount;
+        _allowances[_from][msg.sender] -= _amount;
+
+        emit Transfer(_from, _to, _amount);
+        return true;
+    }
+
+    function mint(address _to, uint256 _amount) public {
+        _totalSupply += _amount;
+        _balances[_to] += _amount;
+
+        emit Transfer(address(0), _to, _amount);
     }
 }
--- test/ERC20Scratch.t.sol
+++ test/ERC20Scratch.t.sol
@@ -31,4 +31,65 @@ contract ERC20Test is Test {
     function test_getBalanceZeroAddress() external view {
         assertEq(erc20.balanceOf(address(0)), 0, "balance mismatch");
     }
+
+    function test_mint1m() external {
+        erc20.mint(address(this), 1_000_000);
+        assertEq(erc20.balanceOf(address(this)), 1_000_000, "balance mismatch");
+    }
+
+    function testFail_transferInsufficientBalance() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.transfer(address(1), 1_000_001);
+    }
+
+    function testFail_transferToZeroAddress() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.transfer(address(0), 100);
+    }
+
+    function test_transfer() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.transfer(address(1), 100);
+        assertEq(erc20.balanceOf(address(this)), 999_900, "balance mismatch");
+        assertEq(erc20.balanceOf(address(1)), 100, "balance mismatch");
+    }
+
+    function test_approveAndAllowance() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.approve(address(1), 100);
+        assertEq(erc20.allowance(address(this), address(1)), 100, "allowance mismatch");
+    }
+
+    function testFail_transferFromInsufficientBalance() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.approve(address(1), 1_000_000);
+        erc20.transferFrom(address(this), address(2), 1_000_001);
+    }
+
+    function testFail_transferFromInsufficientAllowance() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.approve(address(1), 100);
+        erc20.transferFrom(address(this), address(2), 101);
+    }
+
+    function testFail_transferFromToZeroAddress() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.approve(address(1), 100);
+        erc20.transferFrom(address(this), address(0), 100);
+    }
+
+    function test_transferFrom() external {
+        erc20.mint(address(this), 1_000_000);
+        erc20.approve(address(1), 100);
+        vm.prank(address(1));
+        erc20.transferFrom(address(this), address(2), 100);
+        assertEq(erc20.balanceOf(address(this)), 999_900, "balance mismatch");
+        assertEq(erc20.balanceOf(address(2)), 100, "balance mismatch");
+    }
+
+    function test_mint() external {
+        erc20.mint(address(1), 1_000_000);
+        assertEq(erc20.totalSupply(), 1_000_000, "total supply mismatch");
+        assertEq(erc20.balanceOf(address(1)), 1_000_000, "balance mismatch");
+    }
 }
